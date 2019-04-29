4eab688656c98d8676765cfab1e988ebc6f6c625
stefian <v_ianta@yahoo.com>
2019-04-29 17:53:11 -0400

    added web socket.io

--- app.js
+++ app.js
@@ -64,6 +64,9 @@ mongoose.connect(
   { useNewUrlParser: true }
   ).then(result => {
     const server = app.listen(8080);
-    const io = require('socket.io');
+    const io = require('socket.io')(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   }).catch(err => console.log(err));
 
