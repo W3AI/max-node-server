18b6c7cd8686dacd2593ca75e5914d59630352be
stefian <v_ianta@yahoo.com>
2019-04-30 10:52:15 -0400

    added create user in mutation resolver

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const graphqlHttp = require('express-graphql');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 const app = express();
 
@@ -45,6 +49,15 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(
+    '/graphql', 
+    graphqlHttp({
+        schema: graphqlSchema,
+        rootValue: graphqlResolver,
+        graphiql: true
+    })
+);
+
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
@@ -57,6 +70,6 @@ mongoose.connect(
   'mongodb+srv://steve_ai91:o82HN0KCxu6dtS5w@cluster0-n7ze3.mongodb.net/messages?retryWrites=true',
   { useNewUrlParser: true }
   ).then(result => {
-    capp.listen(8080);
+    app.listen(8080);
   }).catch(err => console.log(err));
 
