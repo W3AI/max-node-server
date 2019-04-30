cabbf4caf77eb9a90193bb47a56a27c62cbdf207
stefian <v_ianta@yahoo.com>
2019-04-30 08:23:22 -0400

    removed socket.io, routes, etc in prep for graphQL

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
-
 const app = express();
 
 const fileStorage = multer.diskStorage({
@@ -48,9 +45,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
-
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
@@ -63,10 +57,6 @@ mongoose.connect(
   'mongodb+srv://steve_ai91:o82HN0KCxu6dtS5w@cluster0-n7ze3.mongodb.net/messages?retryWrites=true',
   { useNewUrlParser: true }
   ).then(result => {
-    const server = app.listen(8080);
-    const io = require('./socket').init(server);
-    io.on('connection', socket => {
-      console.log('Client connected');
-    });
+    capp.listen(8080);
   }).catch(err => console.log(err));
 
