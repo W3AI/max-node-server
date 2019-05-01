61c5917a276a0b1087c7a9f68df6a0d09cc83b83
stefian <v_ianta@yahoo.com>
2019-05-01 11:30:11 -0400

    added pagination

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -105,15 +105,21 @@ module.exports = {
             updatedAt: createdPost.updatedAt.toISOString()
         };
     },
-    posts: async function(srgs, req) {
+    posts: async function({page}, req) {
         if (!req.isAuth) {
             const error = new Error('Not authenticated!');
             error.code = 401;
             throw error;
         }
+        if (!page) {
+            page = 1;
+        }
+        const perPage = 2;
         const totalPosts = await Post.find().countDocuments();
         const posts = await Post.find()
             .sort({ createdAt: -1 })
+            .skip((page - 1) * perPage)
+            .limit(perPage)
             .populate('creator');    // -1 for descending order
         return { posts: posts.map(p => {
             return {...p._doc, 
