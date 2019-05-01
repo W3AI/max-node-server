8abe8ee885b878e6adb5ab34b5942e4d668a5641
stefian <v_ianta@yahoo.com>
2019-05-01 10:22:52 -0400

    added get posts query and resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -104,5 +104,24 @@ module.exports = {
             createdAt: createdPost.createdAt.toISOString(),
             updatedAt: createdPost.updatedAt.toISOString()
         };
+    },
+    posts: async function(srgs, req) {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
+        }
+        const totalPosts = await Post.find().countDocuments();
+        const posts = await Post.find()
+            .sort({ createdAt: -1 })
+            .populate('creator');    // -1 for descending order
+        return { posts: posts.map(p => {
+            return {...p._doc, 
+                _id: p._id.toString(),
+                createdAt: p.createdAt.toISOString(),
+                updatedAt: p.updatedAt.toISOString()
+            };
+        }), totalPosts: totalPosts }
+
     }
 };
